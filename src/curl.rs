eea4efac5a79436c3569eb57177ea9e5e90b91cc
Jean-Christophe Amiel <jeanchristophe.amiel@gmail.com>
2024-07-25 18:34:39 +0200

    Update sample.

---
+++ src/curl.rs
@@ -0,0 +1,23 @@
+use crate::Error;
+use curl::easy::{Easy, HttpVersion};
+use std::time::Duration;
+
+impl From<curl::Error> for Error {
+    fn from(err: curl::Error) -> Self {
+        Error(err.code() as i32)
+    }
+}
+
+pub fn perform_head(url: &str) -> Result<(), Error> {
+    let mut handle = Easy::new();
+    handle.url(url)?;
+    handle.nobody(true)?;
+    handle.http_version(HttpVersion::V3)?;
+    handle.verbose(true)?;
+    handle.timeout(Duration::from_secs(20))?;
+
+    let transfer = handle.transfer();
+    transfer.perform()?;
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::process;
+use std::time::Duration;
+use std::{process, thread};
 
+mod curl;
 mod curlsys;
 
 pub struct Error(i32);
@@ -19,15 +21,25 @@ fn main() {
     let url = &args[2];
     let repeat = args[3].parse::<i32>().unwrap();
 
-    for _ in 0..repeat {
+    for i in 0..repeat {
+        println!("Request {kind} #{i}");
         match kind.as_str() {
             "curl_sys" => {
                 let ret = curlsys::perform_head(url);
                 if let Err(Error(code)) = ret {
+                    eprintln!("Error {code}");
+                    process::exit(code);
+                }
+            }
+            "curl" => {
+                let ret = curl::perform_head(url);
+                if let Err(Error(code)) = ret {
+                    eprintln!("Error {code}");
                     process::exit(code);
                 }
             }
             _ => usage(),
         }
+        thread::sleep(Duration::from_secs(1))
     }
 }
